d3b59a33ffd04ab93740c3983060e5162f746413
pc-user <Stanislav-Mot>
2022-06-09 15:55:42 +0300

    third commit

--- src/main/java/aop/University.java
+++ src/main/java/aop/University.java
@@ -12,7 +12,7 @@ public class University {
     public void addStudents() {
         Student student1 = new Student("Zaur Tregulov", 4, 7.5);
         Student student2 = new Student("Mikhail Ivanov", 2, 8.3);
-        Student student3 = new Student("Elena Sidorova", 1, 9.1);
+        Student student3 = new Student("Alena Sidorowa", 1, 9.1);
         students.add(student1);
         students.add(student2);
         students.add(student3);
